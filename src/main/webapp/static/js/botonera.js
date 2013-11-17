60fafe5a3de7500d8bf38c5fcb138b0ad34df9a1
santiago <santiago@tiendanube.com>
2013-11-17 02:05:07 -0300

    try loading sounds all the time for multiple playing

--- src/main/webapp/static/js/botonera.js
+++ src/main/webapp/static/js/botonera.js
@@ -6,6 +6,21 @@ $(document).ready(function() {
     		e.preventDefault();
     		var buttonId = $(this).attr('id');
     		var audio = $('#'+buttonId+'-button');
+    		
+    		console.info(audio[0].canPlayType('audio/mpeg'));
+    		console.info(audio[0].canPlayType('audio/ogg'));
+    		console.info(audio[0].canPlayType('audio/wav'));
+    		
+//    		if (audio.canPlayType('audio/mpeg;')) {
+//    			console.log("holaaa")
+//    		    source.type= 'audio/mpeg';
+//    		    source.src= 'audio/song.mp3';
+//    		} else {
+//    		    source.type= 'audio/ogg';
+//    		    source.src= 'audio/song.ogg';
+//    		}
+    		
+    		audio[0].load();
     		audio[0].play();
     	});
 });
